6a600766e54b135cb5fce449d7402166dee29340
asus <asus@Swapnil05>
2024-01-19 16:44:27 -0500

    first commit

---
+++ src/student.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author asus
+ */
+public class student {
+   
+    private int sId;
+    private String sName;
+    public int getsId() {
+        return sId;
+    }
+    public student(int sId, String sName) {
+        this.sId = sId;
+        this.sName = sName;
+    }
+    public void setsId(int sId) {
+        this.sId = sId;
+    }
+    public String getsName() {
+        return sName;
+    }
+    public void setsName(String sName) {
+        this.sName = sName;
+    }
+    
+}
+
+    
---
+++ src/studentList.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author asus
+ */
+public class studentList {
+    public static void main(String[] args) {
+        student[] studentList=new student[3];
+        student s1=new student(1,"swapnil");
+        studentList[0]=s1;
+        studentList[1]=new student(2, "jack");
+        studentList[2]=new student(3, "Matt");
+
+        for(int i=0;i<studentList.length;i++){
+            System.out.println(studentList[i].getsName());
+        }
+        
+
+    }
+    
+}
