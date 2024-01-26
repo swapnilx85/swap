7b9c11a1d8016dd1c3aa234fe5115006fe750709
asus <asus@Swapnil05>
2024-01-26 16:02:40 -0500

    third commit

--- src/student.java
+++ src/student.java
@@ -11,8 +11,20 @@ public class student {
    
     private int sId;
     private String sName;
+    private String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+
     public int getsId() {
         return sId;
+        
     }
     public student(int sId, String sName) {
         this.sId = sId;
